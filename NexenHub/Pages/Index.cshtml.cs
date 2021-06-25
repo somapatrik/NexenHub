3c1a8ad191bf6afb85e2d290913b5593d1f26730
Patrik Šoma <soma.patrik@gmail.com>
2021-06-25 17:03:40 +0200

    GT prod

--- NexenHub/Pages/Index.cshtml.cs
+++ NexenHub/Pages/Index.cshtml.cs
@@ -9,6 +9,7 @@ using NexenHub.Class;
 using Oracle.ManagedDataAccess.Client;
 using System.Data;
 using NexenHub.Models;
+using System.Text;
 
 namespace NexenHub.Pages
 {
@@ -22,11 +23,44 @@ namespace NexenHub.Pages
         }
 
         public string Production { get; set; }
-                
+        public string MaxValue { get; set; }
+        public string ActValue { get; set; }
+
+
         public void OnGet()
         {
-            Esl epaper = new Esl();
-            epaper.CART_ID = "E1080";
+            MaxValue = "15000";
+            GetProd();
+        }
+
+        public void GetProd()
+        {
+            StringBuilder query = new StringBuilder();
+            query.Append("select /*+index (tb_pr_m_prod IX_PR_M_PROD_5)*/ count(*)");
+            query.AppendLine("from TB_PR_M_PROD");
+            query.AppendLine("where WC_ID = 'T'");
+            query.AppendLine("AND USE_YN = 'Y'");
+            query.AppendLine("AND PLANT_ID = 'P500'");
+            query.AppendLine("AND SHIFT is not null");
+            query.AppendLine("AND PROD_DATE = (select /*+index (tb_pr_m_prod IX_PR_M_PROD_5)*/  max(prod_date) from TB_PR_M_PROD where plant_id = 'P500' and shift is not null and wc_id is not null)");
+
+            DBOra db = new DBOra(query.ToString()) ;
+            DataTable dt = db.ExecTable();
+            if (dt.Rows.Count > 0)
+            {
+                Production = dt.Rows[0][0].ToString();
+                GetProc();
+            }
+                
+        }
+
+        public void GetProc()
+        {
+            double max = double.Parse(MaxValue);
+            double prod = double.Parse(Production);
+            double proc = (prod/max) * 100;
+            string final = Math.Round(proc).ToString();
+            ActValue = final;
         }
 
 
