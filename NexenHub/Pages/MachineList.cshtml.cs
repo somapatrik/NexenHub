49530cc02ab1a219a0e70e94905449aa80a0b4d2
Patrik Šoma <soma.patrik@gmail.com>
2021-08-12 15:09:59 +0200

    Nevim

--- NexenHub/Pages/MachineList.cshtml.cs
+++ NexenHub/Pages/MachineList.cshtml.cs
@@ -11,15 +11,42 @@ namespace NexenHub.Pages
 {
     public class MachineListModel : PageModel
     {
-
-        public DataTable DbList { get; set; }
-
+        public DataTable DbData { get; set; }
+        public List<MachineListObject> DbList;
 
         private GlobalDatabase dbglobal = new GlobalDatabase();
 
+
         public void OnGet()
         {
-            DbList = dbglobal.GetMachineList();
+            DbData = dbglobal.GetMachineList();
+            CreateList();
         }
+
+        private void CreateList()
+        {
+            DataTable dt = DbData;
+            DbList = new List<MachineListObject>();
+            foreach (DataRow r in dt.Rows)
+            {
+                DbList.Add(new MachineListObject
+                {
+                    EQ_ID = r["EQ_ID"].ToString(), 
+                    Name = r["Name"].ToString(),
+                    WC_ID = r["WC_ID"].ToString(),
+                    PROC_ID = r["PROC_ID"].ToString()
+                });
+            }
+        }
+
     }
+
+    public class MachineListObject
+    {
+        public string EQ_ID { get; set; }
+        public string Name { get; set; }
+        public string WC_ID { get; set; }
+        public string PROC_ID { get; set; }
+    }
+
 }
