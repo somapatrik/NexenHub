f61abf80f72218c83231db154137096b8361c54a
Patrik Šoma <soma.patrik@gmail.com>
2022-08-23 10:15:08 +0200

    EMR model

---
+++ NexenHub/Models/EMR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexenHub.Models
+{
+    public class EMR
+    {        
+        public string REQ_NO { get; set; }
+        public string TIRE_CODE { get; set; }
+        public string PATTERN { get; set; }
+        public string TIRE_SIZE { get; set; }
+        public string MOLD_CONT { get; set; }
+        public string BLADDER { get; set; }
+        public string SWI_FLAG { get; set; }
+        public string COMP_CSV { get; set; }
+        public string TOOL { get; set; }
+        public string PKX_TR { get; set; }
+        public string PKX_SW { get; set; }
+        public string BP_CORD { get; set; }
+        public string SB_CORD { get; set; }
+        public string CAP_CORD { get; set; }
+        public string VER_CNT { get; set; }
+        public string REQ_QTY { get; set; }
+        public string WH_FLAG { get; set; }
+        public string DEV_YMD { get; set; }
+        public string CHASU { get; set; }
+        public string TEST_TYPE { get; set; }
+        public string REQ_TEXT { get; set; }
+        public string TEST_SALES_CH { get; set; }
+        public string REQ_YMD { get; set; }
+    }
+}
