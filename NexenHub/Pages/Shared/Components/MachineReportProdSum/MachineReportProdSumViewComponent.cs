f6104bf6e66a78d003204160290a14c4ecf8f58e
Patrik Šoma <soma.patrik@gmail.com>
2022-09-30 16:34:08 +0200

    jhgfhj

---
+++ NexenHub/Pages/Shared/Components/MachineReportProdSum/MachineReportProdSumViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexenHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace NexenHub.Pages.Shared.Components.MachineReportProdSum
+{
+    public class MachineReportProdSumViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(MachineProdReport reportData)
+        {
+            MachineProdReport ReportProd = reportData;
+            return View("Default", ReportProd);
+        }
+    }
+}
