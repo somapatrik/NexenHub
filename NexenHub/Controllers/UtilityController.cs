156adb8fea14e6b5a7bd2821437f5878a01fa464
Patrik Šoma <soma.patrik@gmail.com>
2022-11-21 15:43:02 +0100

    API status

---
+++ NexenHub/Controllers/UtilityController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NexenHub.Class;
+using System;
+using System.Data;
+
+namespace NexenHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilityController : Controller
+    {
+
+        [HttpGet("APIStatus")]
+        public ActionResult<APIStatus> GetApiStatus()
+        {
+            return new APIStatus();
+        }
+
+        public class APIStatus
+        {
+            public bool API { get; set; }
+            public bool DB { get; set; }
+
+            public APIStatus()
+            {
+                API = true;
+                TestDB();
+            }
+
+            private void TestDB()
+            {
+                try 
+                { 
+                    DBOra db = new DBOra("SELECT 1 FROM DUAL");
+                    DB = db.ExecTable().Rows[0][0].ToString() == "1";
+                }
+                catch{}
+            }
+        }
+
+    }
+}
