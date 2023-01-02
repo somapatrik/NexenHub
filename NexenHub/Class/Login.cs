51f43c32c050ad75a6bd6b962eba7d7e4fe5d52b
Patrik Šoma <soma.patrik@gmail.com>
2023-01-02 15:34:08 +0100

    Login static class

--- NexenHub/Class/Login.cs
+++ NexenHub/Class/Login.cs
@@ -1,24 +1,76 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using NexenHub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NexenHub.Class
 {
-    public class Login
+    public static class Login
     {
 
-        public bool IsLogged { get; set; }
+        public static bool IsLogged { get; set; }
 
-        //public bool Login(string username, string password)
-        //{
+        public static async Task<bool> LogIn(HttpContext context, string username, string password)
+        {
+            bool logged = false;
+            GlobalDatabase db = new GlobalDatabase();
+            User user = db.Login(username, password);
 
-        //}
+            if (!string.IsNullOrEmpty(user.UserId))
+            {
 
-        //public void Logout()
-        //{
+                var claims = new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name, user.Name),
+                            new Claim("USER_ID", user.UserId),
+                            new Claim(ClaimTypes.Role, "Administrator"),
+                        };
 
-        //}
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    //AllowRefresh = <bool>,
+                    // Refreshing the authentication session should be allowed.
+
+                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                    // The time at which the authentication ticket expires. A 
+                    // value set here overrides the ExpireTimeSpan option of 
+                    // CookieAuthenticationOptions set with AddCookie.
+
+                    //IsPersistent = true,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+
+                    //IssuedUtc = <DateTimeOffset>,
+                    // The time at which the authentication ticket was issued.
+
+                    //RedirectUri = <string>
+                    // The full path or absolute URI to be used as an http 
+                    // redirect response value.
+                };
+
+
+                await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+            }
+
+            return logged;
+        }
+
+        public static async Task Logout(HttpContext context)
+        {
+            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        }
 
     }
 }
