8b34858aab56a5b77e40ab2c5ce5ee0c9c803046
Patrik Šoma <soma.patrik@gmail.com>
2023-04-24 12:11:07 +0200

    TBM cockpit

---
+++ NexenHub/Pages/Cockpit/tbm.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NexenHub.Class;
+using System.Collections.Generic;
+using System.Data;
+
+namespace NexenHub.Pages.Cockpit
+{
+    public class tbmModel : PageModel
+    {
+        public List<MachineBasicInfo> Phase1 { get; set; }
+        public List<MachineBasicInfo> Phase2 { get; set; }
+
+        private GlobalDatabase database = new GlobalDatabase();
+
+        public void OnGet()
+        {
+            LoadPhase1();
+        }
+
+        public void LoadPhase1()
+        {
+            Phase1 = new List<MachineBasicInfo>(); 
+            Phase2 = new List<MachineBasicInfo>();
+
+            foreach (DataRow row in database.GetMachineList(WC_ID: "T", FACT_ID: "NEX1").Rows)
+            {
+                Phase1.Add(new MachineBasicInfo(row["EQ_ID"].ToString()));    
+            }
+
+            foreach (DataRow row in database.GetMachineList(WC_ID: "T", FACT_ID: "NEX2").Rows)
+            {
+                Phase2.Add(new MachineBasicInfo(row["EQ_ID"].ToString()));
+            }
+        }
+    }
+}
